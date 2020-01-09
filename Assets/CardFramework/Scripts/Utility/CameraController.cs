3c880cfe3caab1c2e9b4f69f380f1ef0fc328d15
Will Webster <williamjswebster@hotmail.co.uk>
2020-01-09 21:25:09 +0000

    Initial Commit
    (this time with files)

---
+++ Assets/CardFramework/Scripts/Utility/CameraController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform[] views;
+    public float transitionSpeed;
+    Transform currentView;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentView
+    }
+
+    void Update()
+    {
+        // reset to original view
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            currentView = views[0];
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            currentView = views[1];
+        }
+
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            currentView = views[2];
+        }
+
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            currentView = views[3];
+        }
+
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            currentView = views[4];
+        }
+
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+        Vector3 currentAngle = new Vector3(
+            Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+            Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+            Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+        transform.eulerAngles = currentAngle;
+    }
+
+    // LateUpdate to be used in cases of linear interpolation
+    void LateUpdate()
+    {
+        //transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+        //Vector3 currentAngle = new Vector3(
+        //    Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+        //    Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+        //    Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+        //transform.eulerAngles = currentAngle;
+    }
+}
---
+++ Assets/CardFramework/Scripts/Utility/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(target);
+
+#if UNITY_EDITOR
+        transform.LookAt(target);
+#endif
+    }
+}
